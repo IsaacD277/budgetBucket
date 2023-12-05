100dc3cde17c3cbd78d69bc2a35fc927ef33881a
Isaac D2 <idarr08@gmail.com>
2023-12-05 17:20:48 -0500

    Initial Commit

---
+++ budgetBrain/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  budgetBrain
+//
+//  Created by Isaac D2 on 12/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ budgetBrain/budgetBrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  budgetBrainApp.swift
+//  budgetBrain
+//
+//  Created by Isaac D2 on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct budgetBrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
