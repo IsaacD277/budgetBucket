0a12449a85260ea0c04e4800b539613026466c29
Isaac D2 <idarr08@gmail.com>
2023-12-26 18:16:44 -0500

    Initial sprint branch commit

---
+++ budgetBrain/transactionDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  transactionDetailView.swift
+//  budgetBrain
+//
+//  Created by Isaac D2 on 12/24/23.
+//
+
+import SwiftUI
+
+struct transactionDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    transactionDetailView()
+}
